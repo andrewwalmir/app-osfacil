fda6b6887624666c6d256e98472ab19940534ac2
andrewwalmir <andrewwalmir@gmail.com>
2019-04-07 14:51:22 -0300

    lazy loading

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular/navigation/ionic-page';
+
+@IonicPage()
 
 @Component({
   selector: 'page-home',
