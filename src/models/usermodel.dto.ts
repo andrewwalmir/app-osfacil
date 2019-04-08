ab5dd02740a5b3d197ed16f19cd09d98b2bf0678
Matielo J Geronimo <matielojg@gmail.com>
2019-04-08 02:56:44 -0300

    criei a UserModel

---
+++ src/models/usermodel.dto.ts
@@ -0,0 +1,10 @@
+export interface UserModelDTO {
+        id : number;
+        nome : string;
+        login: string;
+	    senha: string;
+	    email: string;
+	    foto: string;
+	    cpf: string;
+	    contact: string;
+}
