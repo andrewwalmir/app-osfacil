a413b4c2f9ad46e213a2322182a168873733bb0d
Matielo J Geronimo <matielojg@gmail.com>
2019-04-27 02:36:36 -0300

    alterar usuario Logado fazendo list setor e funçao

--- src/pages/usuario/profile/profile.ts
+++ src/pages/usuario/profile/profile.ts
@@ -1,26 +1,83 @@
-
+import { UserModelDTO } from './../../../models/usermodel.dto';
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController,
+  LoadingController
+} from "ionic-angular";
 import { ConfigService } from "../../../services/config.service";
-import { NavLifecycles } from  '../../../utils/ionic/nav/nav-lifecycles';
+import { NavLifecycles } from "../../../utils/ionic/nav/nav-lifecycles";
+import { ProfileService } from "../../../services/profile.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { SectorModelDTO } from "../../../models/sectorModel.dto";
+import { FunctionModelDTO } from "./../../../models/functionModel.dto";
 @IonicPage()
-
 @Component({
   selector: "page-profile",
   templateUrl: "profile.html"
 })
-export class ProfilePage implements NavLifecycles{
+export class ProfilePage implements NavLifecycles {
+  public functions: FunctionModelDTO[];
+  public sectors: SectorModelDTO[];
+  public alterarUsuario : UserModelDTO;  
   
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public configService: ConfigService
-    
-    ) {}
+    public profileService: ProfileService,
+    public configService: ConfigService,
+    private _alertCtrl: AlertController,
+    private _loadingCtrl: LoadingController
+  ) {}
+  //https://cursos.alura.com.br/course/ionic3-parte1/task/33246
+  ionViewDidLoad() {
+    let loading = this._loadingCtrl.create({
+      content: "Carregando Funções..."
+    });
 
- ionViewDidLoad() {
-    console.log("ionViewDidLoad ProfilePage");
+    this.profileService.listarFunction().subscribe(
+      functions => {
+        this.functions = functions;
+        loading.dismiss(); //sumir o loading quando carregar o componente por completo
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err);
+        this._alertCtrl
+          .create({
+            title: "Falha na conexão",
+            subTitle:
+              "Não foi possível carregar a lista de funções. Tente novamente mais tarde!",
+            buttons: [{ text: "Ok" }]
+          })
+          .present();
+      }
+    );
+
+    this.profileService.listarSector().subscribe(
+      sectors => {
+        this.sectors = sectors;
+        loading.dismiss(); //sumir o loading quando carregar o componente por completo
+      },
+      (err: HttpErrorResponse) => {
+        this._alertCtrl
+          .create({
+            title: "Falha na conexão",
+            subTitle:
+              "Não foi possível carregar a lista de funções. Tente novamente mais tarde!",
+            buttons: [{ text: "Ok" }]
+          })
+          .present();
+      }
+    );
+      
+    }
+
+    //changeUser(){
+      //  console.log(this.)
+   // }
+
   }
 
- 
-}
--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -1,14 +1,25 @@
+import { SectorModelDTO } from "./../models/sectorModel.dto";
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { JwtHelper } from "angular2-jwt";
+import { FunctionModelDTO } from "../models/functionModel.dto";
+import { API_CONFIG } from "../config/api.config";
 
 @Injectable()
-export class ProfileService{
+export class ProfileService {
+  jwtHelper: JwtHelper = new JwtHelper();
+  get: any;
 
-    jwtHelper: JwtHelper = new JwtHelper();
-    constructor(private _http: HttpClient){
+  constructor(private _http: HttpClient) {}
 
-    }
-
-    
+  listarFunction() {
+    return this._http.get<FunctionModelDTO[]>(
+      `${API_CONFIG.baseUrl}/OSFacil_Back/api/function/listarFunction`
+    );
+  }
+  listarSector() {
+    return this._http.get<SectorModelDTO[]>(
+      `${API_CONFIG.baseUrl}/OSFacil_Back/api/sector/listar`
+    );
+  }
 }
