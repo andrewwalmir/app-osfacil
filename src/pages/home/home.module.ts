d2213484f3c71315144919c34d8cd8ec2786bdc3
andrewwalmir <andrewwalmir@gmail.com>
2019-05-21 21:44:53 -0300

    Alteraçoes na Dashboard, Listar Ordens de Serviço e Listar Usuarios

--- src/pages/home/home.module.ts
+++
@@ -1,9 +0,0 @@
-import { IonicPageModule } from 'ionic-angular/module';
-import { NgModule } from '@angular/core';
-import { HomePage } from './home';
-@NgModule({
-    declarations: [HomePage],
-    imports: [IonicPageModule.forChild(HomePage)]
-})
-export class HomeModule {
-}
